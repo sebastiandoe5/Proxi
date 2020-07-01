012ff18999ab67ddcec1f6c958047e2bde98ce00
Sebastian Doe <sebastiandoe5@gmail.com>
2020-07-01 14:50:14 +0100

    Test PWA Development

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ $(function() {
         $(".ios").show();
     }
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(function(registration) {
+            console.log('Service Worker Registered');
+      });
+    navigator.serviceWorker.ready.then(function(registration) {
+       console.log('Service Worker Ready');
+    });
+}
